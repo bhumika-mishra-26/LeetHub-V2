d404e3d38f6a2e03ab19d6cb58b643d677436a7e
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-07-21 14:26:44 +0530

    Time: 2 ms (89.01%), Space: 47.9 MB (43.37%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+     
+        int maxi=0;
+
+        for(int i=0;i<n;i++)
+        {
+if(i>maxi)
+return false;
+maxi=Math.max(nums[i]+i,maxi);
+
+
+        }
+        return maxi>=n-1;
+
+        
+    }
+}
