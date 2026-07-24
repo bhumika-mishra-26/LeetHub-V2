1f2994da6f8c9673fcc94d676d8507becc9663cf
Bhumika Mishra <2k23.csai2310288@gmail.com>
2026-07-24 12:33:10 +0530

    Time: 48 ms (54.85%), Space: 47.6 MB (53.92%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,44 +1,36 @@
 class Solution {
-
     public boolean canPartition(int[] nums) {
-
         int n = nums.length;
         int sum = 0;
 
-        for(int x : nums)
-            sum += x;
+        for (int i : nums) {
+            sum += i;
 
-        // If total sum is odd partition is impossible
-        if(sum % 2 != 0)
+        }
+        if (sum % 2 != 0)//agar divide nhi ho rha h to pakka false hi hoga 
             return false;
+        int amount = sum / 2;
+        // har ek subset ka sum 
+        boolean[][] dp = new boolean[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
 
-        int s = sum / 2;
+        }
+        for (int i = 0; i <= amount; i++) {
+            dp[0][i] = false;
 
-        boolean[][] dp = new boolean[n+1][s+1];
-
-      
-        for(int i=0;i<=n;i++)
-            dp[i][0]=true;
-
-        for(int j=1;j<=s;j++)
-            dp[0][j]=false;
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=s;j++)
-            {
-                if(nums[i-1] > j)
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j < nums[i - 1])
+                    dp[i][j] = dp[i - 1][j];
                 else
-                {
-                    dp[i][j]=dp[i-1][j] ||
-                             dp[i-1][j-nums[i-1]];
-                }
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+
             }
         }
 
-        return dp[n][s];
+        return dp[n][amount];
+
     }
 }
